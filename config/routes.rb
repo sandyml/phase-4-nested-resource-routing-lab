49e2292405c2146b8dfb214f7b3102f1471279f7
sandym <91996681+sandyml@users.noreply.github.com>
2023-01-25 15:21:39 -0500

    commit tree 5ceb4b4003be2270bdd8293bdf495a25fce83275

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  
+  # NESTED 
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :show, :create]
+  end
+  
 end
+
